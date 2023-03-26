5045f832dc1a19ff54eb2a9699b19e62ec8fc76a
noamhertz <hertz.noam@gmail.com>
2023-03-27 00:53:57 +0300

    good popup

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './popup.scss';
 
-const test = <img src="icon.png" />;
+const popupIcon = <img src="popup-icon.png" />;
 
 const entry = document.createElement('div');
+entry.onclick = () => {
+	window.open('https://github.com/INT-Calutt/buddy', '_blank');
+};
 entry.id = 'entry';
 document.body.appendChild(entry);
 const root = createRoot(document.getElementById('entry'));
-root.render(test);
+root.render(popupIcon);
